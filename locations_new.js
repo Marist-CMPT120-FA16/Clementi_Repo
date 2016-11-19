8e7ea3c325ce6bb31459ce26fe8619cfcdc6ed4f
johnatawnclementawn <JohnathanClementi@148-100-171-103.FoxNet.marist.edu>
2016-11-19 15:53:32 -0500

    Project 5 v0.4
    Added new trail function, location array, location matrix for level
    handling, take function (not functional yet)
    
    To do:
    organize code so that pieces flow better… not all over the place (so I
    can find stuff.)
    fix functionality of button handlers, matrix, and inventory readout
    
    also add highlighted locations on map

--- locations_new.js
+++ locations_new.js
@@ -22,14 +22,17 @@
     var score = 0;
     var btnErrorCount = 0;
     var limit = 5;
+    var direction = 0
+// Inventory hold - open spaces for items
+    var inventory = [""];
 
 //Initialization Function - boots up program
   function init() {
-    updateDisplay ("To begin your trek at Philmont Scout Ranch, click any button!");
+    updateDisplay ("To begin your trek at Philmont Scout Ranch, click a button or input a direction and click 'Lets Hike!'!");
     document.getElementById("btnNorth").disabled = false;
     document.getElementById("btnSouth").disabled = false;
-    document.getElementById("btnEast").disabled = true;
-    document.getElementById("btnWest").disabled = false;
+    document.getElementById("btnEast").disabled = false;
+    document.getElementById("btnWest").disabled = true;
   }
 
 
@@ -40,7 +43,7 @@
   }
 
 //Help function - gives user valid inpunts and commands
-    function btnHelp(){
+  function btnHelp(){
       updateDisplay ("The valid moves are North, South, East, West, N, S, E, W." +
                       "\n\n" + "Or use the buttons below" + "\n" + "Commands are 'Lets Hike', 'Help', 'Take' and 'Inventory.'"
                       + "\n" + "Press 'Lets Hike' to input your directional command."
@@ -56,13 +59,13 @@
 
 
 // Location prototype - item object contains six traits - id, name, description, description after visited, item, and if the loc was visited.
-function location (id, name, desc, descVisited, item){
-  this.id = id;
-  this.name = name;
-  this.desc = desc;
-  // this.descVisited = descVisited;
-  this.item = item;
-  this.visited = false;
+  function location (id, name, desc, descVisited, item){
+    this.id = id;
+    this.name = name;
+    this.desc = desc;
+ // this.descVisited = descVisited;
+    this.item = item;
+    this.visited = false;
 
   function toString(){
     return desc;
@@ -81,7 +84,7 @@ function location (id, name, desc, descVisited, item){
   var cimmarroncito = new location (8, "Cimmarroncito staff camp", "Otherwise known as 'Cito', this one of the largest and most central camps on the ranch. Cito also has outdoor rock climbing as well as an indoor bouldering gym", null);
   var vistoGrande = new location (9, "Visto Grande trail camp", "This camp has a beautiful view of the hike over the next few days.", null);
   var headOfDean = new location (10, "Head of Dean staff camp", "HOD is a challenge course camp that helps your crew come together as a team.  The staff have new boots here for you!", boots);
-
+  var locations = [baseCamp,loversLeap,minersPark,craterLake,toothOfTime,porcupine,clearCreek,mountPhilips,cimmarroncito,vistoGrande,headOfDean];
 // Item function - item object contains three traits - id, name, description.
   function Item (id,name,desc) {
       this.id = id;
@@ -102,32 +105,102 @@ function location (id, name, desc, descVisited, item){
     var naglene = new item (4, "Nalgene water bottle", "These water bottles hold a litre of water each!");
     var crewFlag = new item (5, "Crew Leader Flag", "This American Flag is attached to the crew leader's backpack")
 
-
-
+// matrix
+    var matrix = [
+    // baseCamp       [N,E,S,W]
+                      [0,0,1,2],
+    // loversLeap
+                      [0,0,0,0],
+    // minersPark
+                      [0,0,0,0],
+    // craterLake
+                      [0,0,0,0],
+    // toothOfTime
+                      [0,0,0,0],
+    // porcupine
+                      [0,0,0,0],
+    // clearCreek
+                      [0,0,0,0],
+    // mountPhilips
+                      [0,0,0,0],
+    // cimmarroncito
+                      [0,0,0,0],
+    // vistoGrande
+                      [0,0,0,0],
+    // headOfDean
+                      [0,0,0,0],
+    ];
 
 // Button Event Function Handlers
-// Click event for each button codes for 1 column of events in map matrix
+// Click event for each button codes for 1 column of events in matrix
 // i.e. North = 1st column; East = 2nd column; South = 3rd column; West = 4th column
-    function btnNorth(){
-      currentLvl = map[currentLvl][0];
+  function btnNorth(){
+      currentLvl = matrix[currentLvl][0];
+      trail();
+    }
+  function btnEast(){
+      currentLvl = matrix[currentLvl][1];
+      trail();
+    }
+  function btnSouth(){
+      currentLvl = matrix[currentLvl][2];
+      trail();
+    }
+  function btnWest(){
+      currentLvl = matrix[currentLvl][3];
       trail();
     }
 
-    function btnEast(){
-      currentLvl = map[currentLvl][1];
-      trail();
-    }
 
-    function btnSouth(){
-      currentLvl = map[currentLvl][2];
-      trail();
-    }
+// Trail function is no longer switch case; simple else if statement that determines what level the user is at
+// Then it looks to see if the user has been at that location. If they have, no score is added, if not, 5pts is added to the score
+// Then the buttons are disabled based on what actions can be carried out
+  function trail(){
+    var loc = location[currentLvl];
+    var msg = loc.name + ":" + "\n" + loc.desc;
 
-    function btnWest(){
-      currentLvl = map[currentLvl][3];
-      trail();
-    }
+    if (loc.visited === false){
+      score += 5;
+      loc.visited = true;
+  }
+    document.getElementById("btnNorth").disabled = false;
+    document.getElementById("btnEast").disabled = false;
+    document.getElementById("btnSouth").disabled = true;
+    document.getElementById("btnWest").disabled = false;
 
+    if (loc.id === matrix[currentLvl[0]]){
+    document.getElementById("btnNorth").disabled = true;
+  } if (loc.id === matrix[currentLvl[1]]){
+    document.getElementById("btnEast").disabled = true;
+  } if (loc.id === matrix[currentLvl[2]]){
+    document.getElementById("btnSouth").disabled = true;
+  } if (loc.id === matrix[currentLvl[3]]){
+    document.getElementById("btnWest").disabled = true;
+  }
+    updateDisplay (msg);
+}
+
+//Take function evaluates whether or not there is an item at the location
+//If not, it tells user that there is no item
+//If there is, it 'takes' the item and puts it in the user's inventory and then removes it from the location so that if you visit the location again, the item wont be there
+  function btnInvTake(){
+    var loc = location[currentLvl];
+    if (loc.item === null){
+      updateDisplay ("There are no items at this camp!")
+    }
+    else {
+      var item = loc.item;
+      inventory[item.id] = item;
+      loc.item = null;
+   // loc.desc = loc descVisited;
+   // updateDisplay ("Congrats! You recieved: "+ item.name);
+    }
+}
+
+function btnInvList(){
+	var message = "Your Inventory is:" + inventory;
+  updateDisplay (message);
+}
 
 // User inputed command control
 // Takes user imput from command textbox, converts it to lowercase
